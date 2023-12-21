b6c11c08423a38214a0b550c1e17b748f12cb8d6
eder3232 <eldebar.dk@gmail.com>
2023-12-21 10:14:18 -0500

    Add site configuration and Clerk components

--- src/app/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router'
-
-const Page = ({ params }: { params: { id: string } }) => {
-  return (
-    <div className="flex items-center flex-col justify-center">{params.id}</div>
-  )
-}
-
-export default Page
